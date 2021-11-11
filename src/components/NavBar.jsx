a445723d09b7c8b27e3708aacd01721ba1a5744f
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-11-11 10:33:44 +0530

    Components Addition

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,21 +14,21 @@ const NavBar = () => {
            
         <nav className='navbar navbar-expand-lg navbar-light bg-light' style={{borderRadius: "41px"}}>
         <div className="container-fluid">
-           <Link to='/' className='navbar-brand' id='nav-brand'> <FaShopify /> </Link>
+           <Link to='/' className='navbar-brand' id='nav-brand' style={{paddingTop: 0}}> <FaShopify /> </Link>
            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-           <div className="collapse navbar-collapse nav-final" id="navbarSupportedContent" style={{paddingLeft: '37%'}}>
+           <div className="collapse navbar-collapse nav-final" id="navbarSupportedContent" style={{paddingLeft: '15%'}}>
            <form className="d-flex search">
-        <input name="valData" onChange={() => {}} className="form-control me-2" type="search" placeholder="Search recipes" aria-label="Search" style={{width: "17rem", borderRadius: "25px",background: "white"}} />
+        <input name="valData" onChange={() => {}} className="form-control me-2" type="search" placeholder="Search productss" aria-label="Search" style={{width: "17rem", borderRadius: "25px",background: "white"}} />
         <div type="submit" onClick={() => {}} style={{paddingLeft: "4px" ,fontSize: "23px"}}><BsSearch /></div>
         </form>
-           <ul className='navbar-nav ml-auto mb-2 mb-lg-0'>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/top' activeclassname="menu-active" className="nav-link">MEN</Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/healthy' activeclassname="menu-active" className="nav-link">WOMEN</Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/contact2' activeclassname="menu-active" className="nav-link">KIDS</Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}} id='nav-brand'><Link to='/cart' activeclassname="menu-active" className="nav-link"> <HiShoppingCart /> </Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}} id='nav-brand'><Link to='/profile' activeclassname="menu-active" className="nav-link">  <CgProfile /> </Link><span></span></li>
+           <ul className='navbar-nav ml-auto mb-2 mb-lg-0' style={{marginLeft: '12%', fontSize: '1.15rem', width: '-webkit-fill-available'}}>
+                <li className='nav-item menuitem' style={{marginRight: "22px", width: '20%'}}><Link to='/men' activeclassname="menu-active" className="nav-link" id="nav-category">MEN</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px", width: '20%'}}><Link to='/women' activeclassname="menu-active" className="nav-link" id="nav-category">WOMEN</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px", width: '20%'}}><Link to='/kids' activeclassname="menu-active" className="nav-link" id="nav-category">KIDS</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px"}} id='nav-brand'><Link to='/cart' activeclassname="menu-active" className="nav-link" id="navbar-icons"> <HiShoppingCart /> </Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px"}} id='nav-brand'><Link to='/profile' activeclassname="menu-active" className="nav-link" id="navbar-icons">  <CgProfile /> </Link><span></span></li>
 
             </ul>
             </div>
