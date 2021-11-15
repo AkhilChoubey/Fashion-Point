c74a16124dbb3e78672a40e9adf115ac0aa0e6db
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-11-15 17:24:13 +0530

    Home Component

--- src/components/Carosel.jsx
+++ src/components/Carosel.jsx
@@ -20,10 +20,7 @@ const Carosel = () => {
               background: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${a11})`,
             }}
           >
-            <h2 className="slider-quote">
-              "A RECIPE IS A STORY WHICH ENDS WITH A GOOD MEAL"
-            </h2>
-            <p>-Pat Conroy</p>
+            <h2 className="slider-quote">BUY 1 GET 1 (OFFER EXPIRES SOON)</h2>
           </div>
         </Paper>
         <Paper>
@@ -33,11 +30,7 @@ const Carosel = () => {
               background: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${a12})`,
             }}
           >
-            <h2 className="slider-quote">
-              "A RECIPE HAS NO SOUL. YOU, AS THE COOK, MUST BRING SOUL TO THE
-              RECIPE"
-            </h2>
-            <p>-Thomas Keeler</p>
+            <h2 className="slider-quote">UPTO 80% OFF ON KIDS WEARS</h2>
           </div>
         </Paper>
         <Paper>
@@ -48,24 +41,8 @@ const Carosel = () => {
             }}
           >
             <h2 className="slider-quote">
-              "PUT UP A CHAIR. TAKE A TASTE. COME JOIN US. LIFE IS SO ENDLESSLY
-              DELICIOUS."
+              BEST DEALS OF THE DAY (UPTO 50% OFF ON WOMENS WEAR.)
             </h2>
-            <p>-Ruth Reichl</p>
-          </div>
-        </Paper>
-        <Paper>
-          <div
-            className="slider-div"
-            style={{
-              background: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${a15})`,
-            }}
-          >
-            <h2 className="slider-quote">
-              "PUT UP A CHAIR. TAKE A TASTE. COME JOIN US. LIFE IS SO ENDLESSLY
-              DELICIOUS."
-            </h2>
-            <p>-Ruth Reichl</p>
           </div>
         </Paper>
       </Carousel>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,6 @@ const Home = () => {
       <div className="home-carosel">
         <Carosel />
       </div>
-      <Carosel />
     </div>
   );
 };
