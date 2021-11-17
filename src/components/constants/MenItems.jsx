a859ee9557d49cf4315d280b03fcc958c1b627fd
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-11-18 00:02:05 +0530

    Categories Component

--- src/components/constants/MenItems.jsx
+++ src/components/constants/MenItems.jsx
@@ -3,33 +3,58 @@ const MenItems = [
     id: 1,
     image:
       "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
-    price: "Rs. 999",
-    name: "SHIRT",
-    description: "Clasic Shirt",
+    image2:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/b40e3089-2e16-4982-b6e9-9e5522d6725b1566454086712-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-5.jpg",
+    image3:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/9292c168-14fc-45b0-94d7-442414f8d4f81566454086728-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-4.jpg",
+    price: "Rs. 647",
+    name: "DENNIS LINGO",
+    description: "Men Slim Fit Clasic Shirt",
   },
   {
     id: 2,
     image:
-      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
-    price: "Rs. 1999",
-    name: "SHIRT",
-    description: "Clasic Shirt",
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11780968/2020/4/3/11591b03-97f1-4907-b735-906984c4bcd61585905051096HIGHLANDERMenGreenWhiteSlimFitPrintedCasualShirt1.jpg",
+    image2:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11780968/2020/4/3/11591b03-97f1-4907-b735-906984c4bcd61585905051096HIGHLANDERMenGreenWhiteSlimFitPrintedCasualShirt2.jpg",
+    image3:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11780968/2020/4/3/9f896858-2c78-4a94-b93c-0a6b32f6e8721585905051288HIGHLANDERMenGreenWhiteSlimFitPrintedCasualShirt4.jpg",
+    price: "Rs. 629",
+    name: "HIGHLANDER",
+    description: "Men Slim Fit Casual Shirt",
   },
   {
     id: 3,
     image:
-      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
-    price: "Rs. 999",
-    name: "SHIRT",
-    description: "Clasic Shirt",
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1364628/2016/8/31/11472636737718-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-1.jpg",
+    image2:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1364628/2016/8/31/11472636737718-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-2.jpg",
+    image3:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1364628/2016/8/31/11472636737718-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-3.jpg",
+    price: "Rs. 524",
+    name: "ROADSTER",
+    description: "Men Slim Fit Casual Shirt",
   },
   {
     id: 4,
     image:
-      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
-    price: "Rs. 1999",
-    name: "SHIRT",
-    description: "Clasic Shirt",
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12131944/2020/8/5/0bdb217f-11f7-4f53-b96f-6d54ddeccd0f1596607596194-WROGN-Men-Shirts-4631596607594327-1.jpg",
+    image2:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12131944/2020/8/5/0bdb217f-11f7-4f53-b96f-6d54ddeccd0f1596607596194-WROGN-Men-Shirts-4631596607594327-2.jpg",
+    image3:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12131944/2020/8/5/0bdb217f-11f7-4f53-b96f-6d54ddeccd0f1596607596194-WROGN-Men-Shirts-4631596607594327-3.jpg",
+    price: "Rs. 1649",
+    name: "WROGN",
+    description: "Clasic Shirt Causual Shirt",
+  },
+  {
+    id: 5,
+    image: "",
+    image2: "",
+    image3: "",
+    price: "",
+    name: "",
+    description: "",
   },
 ];
 
