c742799a6868e4a8fa901be6a3ec7d967c3ad1a3
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-11-17 16:26:59 +0530

    Home Component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
-
+import { useCart } from "react-use-cart";
 const Cart = () => {
   const [isLoading, setLoading] = useState(true);
 
@@ -10,8 +10,44 @@ const Cart = () => {
     }, 1000);
   });
 
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    updateItemQuantity,
+    removeItem,
+  } = useCart();
+
   return (
-    <div>{isLoading ? <Loading /> : <h1> This is Cart Component</h1>}</div>
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : isEmpty ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div>
+          <h1>Cart ({totalUniqueItems})</h1>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                {item.quantity} x {item.name} &mdash;
+                <button
+                  onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                >
+                  +
+                </button>
+                <button onClick={() => removeItem(item.id)}>&times;</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Men.jsx
+++ src/components/Men.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
 import { ProductCard } from "react-ui-cards";
+import { useCart } from "react-use-cart";
+import MenItems from "./constants/MenItems";
 const Men = () => {
   const [isLoading, setLoading] = useState(true);
+
+  const { addItem } = useCart();
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,19 +19,39 @@ const Men = () => {
       {isLoading ? (
         <Loading />
       ) : (
-        <ProductCard
-          photos={[
-            "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
-            "https://i.imgur.com/jRVDeI8.jpg",
-            "https://i.imgur.com/raPe27t.jpg",
-            "https://i.imgur.com/IpEsYSH.jpg",
-          ]}
-          price="$99"
-          productName="Headphones"
-          description="Donec lectus nulla, molestie aliquam nisl vitae, tempor placerat magna. Morbi dignissim in felis vel aliquet."
-          buttonText="Add to cart"
-          url="https://github.com/nukeop"
-        />
+        <div>
+          {MenItems.map((m) => (
+            <div>
+              <ProductCard
+                photos={[m.image]}
+                price={m.price}
+                productName={m.name}
+                description={m.description}
+              />
+              <button onClick={() => addItem(m)}>Add to cart</button>
+            </div>
+          ))}
+          <ProductCard
+            photos={[
+              "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
+            ]}
+            price="Rs. 1000"
+            productName="SHIRT"
+            description="Clasic Shirt"
+            buttonText="Add to cart"
+            url="/cart"
+          />
+          <ProductCard
+            photos={[
+              "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg",
+            ]}
+            price="Rs. 1000"
+            productName="SHIRT"
+            description="Clasic Shirt"
+            buttonText="Add to cart"
+            url="https://github.com/nukeop"
+          />
+        </div>
       )}
     </div>
   );
