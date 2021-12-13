c7ced6fd313bee5955731a069df22be3cde86a84
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-14 00:02:07 +0530

    Style Changes

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -113,9 +113,11 @@ const Login = () => {
             >
               LOGIN
             </button>
-            <h6 id="login-redirect-to-signup">
-              New to ShopKeeper? <Link to="/signup">SignUp</Link>
-            </h6>
+            <div style={{ textAlign: "center" }}>
+              <h6 id="login-redirect-to-signup">
+                New to ShopKeeper? <Link to="/signup">SignUp</Link>
+              </h6>
+            </div>
             {/* <Button
             variant="outlined"
             color="primary"
